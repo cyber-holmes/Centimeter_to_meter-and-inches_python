01d9e7eeae622272aef7978a870a9b14775edc52
cyber-holmes <67431536+cyber-holmes@users.noreply.github.com>
2020-12-25 18:35:30 +0530

    Add files via upload

---
+++ height_cm.py
@@ -0,0 +1,17 @@
+#Goal:Convert given Height from Centimeter to Meter and Inches.
+
+#Step1:Take the input.
+height = input("Enter the Height in Centimeter: ")
+
+#Step2:Calculate the value of Meter from Centimeter.
+meter = height/100.0 
+
+#Step3:Calculate the value of Inch from Centimeter.
+inch = height/2.54
+
+#Step4:Print the Height in Meter.
+print "Height in Meter: ",meter,"m"
+
+
+#Step5:Height in Inch.
+print "Height in Inch: ",inch,"in"
